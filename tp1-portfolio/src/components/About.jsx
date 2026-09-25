0348c2a94e8b06ec1f3b0477762a6dcb3811a15e
Sofmarlu <sofymontenegroruiz@gmail.com>
2026-09-25 07:04:26 -0300

    se agregaron props al hero y se corrigio el index.css

---
+++ tp1-portfolio/src/components/About.jsx
@@ -0,0 +1,29 @@
+import "../css/About.css";
+
+function About(props) {
+  return (
+    <>
+      <section className="about">
+        <h2 className="about-titulo">Sobre mi</h2>
+
+        <p className="about-descripcion">{props.descripcion}</p>
+
+        <div className="about-stats">
+          <div className="about-stat">
+            <span className="about-stat-numero">+{props.anios} años</span>
+            <span className="about-stat-label">Estudiando tecnología</span>
+          </div>
+
+          <div className="about-stat">
+            <span className="about-stat-numero">
+              +{props.proyectosRealizados} Proyectos
+            </span>
+            <span className="about-stat-label">Realizados</span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default About;
